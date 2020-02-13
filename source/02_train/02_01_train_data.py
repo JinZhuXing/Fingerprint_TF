c5c767ab27ff560a7eda14c5772964096f13321c
JinZhuXing <peace1019@126.com>
2020-02-13 16:20:56 +0800

    Add train_data

---
+++ source/02_train/02_01_train_data.py
@@ -0,0 +1,174 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import random
+
+import os
+
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("GPU: ", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+
+
+# %%
+# load data
+img_data = np.load('../../dataset/np_data/img_train.npy')
+label_data = np.load('../../dataset/np_data/label_train.npy')
+img_real = np.load('../../dataset/np_data/img_real.npy')
+label_real = np.load('../../dataset/np_data/label_real.npy')
+
+print(img_data.shape, label_data.shape)
+print(img_real.shape, label_real.shape)
+
+
+
+# %%
+# split data
+img_train, img_val, label_train, label_val = train_test_split(img_data, label_data, test_size = 0.1)
+print(img_data.shape, label_data.shape)
+print(img_train.shape, label_train.shape)
+print(img_val.shape, label_val.shape)
+
+
+
+# %%
+# model definition
+def build_model():
+    x1 = layers.Input(shape = (160, 160, 1))
+    x2 = layers.Input(shape = (160, 160, 1))
+
+    # share weights both inputs
+    inputs = layers.Input(shape = (160, 160, 1))
+    feature = layers.Conv2D(32, kernel_size = 3, padding = 'same', activation = 'relu')(inputs)
+    feature = layers.MaxPooling2D(pool_size = 2)(feature)
+    feature = layers.Conv2D(32, kernel_size = 3, padding = 'same', activation = 'relu')(feature)
+    feature = layers.MaxPooling2D(pool_size = 2)(feature)
+    feature_model = Model(inputs = inputs, outputs = feature)
+
+    # two feature models that sharing weights
+    x1_net = feature_model(x1)
+    x2_net = feature_model(x2)
+
+    # subtract features
+    net = layers.Subtract()([x1_net, x2_net])
+    net = layers.Conv2D(32, kernel_size = 3, padding = 'same', activation = 'relu')(net)
+    net = layers.MaxPooling2D(pool_size = 2)(net)
+    net = layers.Flatten()(net)
+    net = layers.Dense(64, activation = 'relu')(net)
+    net = layers.Dense(1, activation = 'sigmoid')(net)
+    model = Model(inputs = [x1, x2], outputs = net)
+
+    # compile
+    model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['acc'])
+
+    # show summary
+    model.summary()
+
+    return model
+
+
+
+# %%
+# data generator definition
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, img_data, label_data, img_real, label_real, batch_size = 32, shuffle = True):
+        'Initialization'
+        self.img_data = img_data
+        self.label_data = label_data
+        self.img_real = img_real
+        self.label_real = label_real
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.img_data) / self.batch_size) * 2)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        real_idx = index
+        index = int(np.floor(index / 2))
+        img1_batch = self.img_data[index * self.batch_size : (index + 1) * self.batch_size]
+        label1_batch = self.label_data[index * self.batch_size : (index + 1) * self.batch_size]
+        img2_batch = np.empty((self.batch_size, 160, 160, 1), dtype = np.float32)
+        label2_batch = np.zeros((self.batch_size, 1), dtype = np.float32)
+
+        for i, idx in enumerate(label1_batch):
+            if random.random() > 0.5:
+                # put matched image
+                img2_batch[i] = self.img_real[idx]
+                label2_batch[i] = 1.
+            else:
+                # put unmatched image
+                while True:
+                    unmatch_idx = random.choice(list(self.label_real))
+                    if (unmatch_idx != idx):
+                        break
+
+                img2_batch[i] = self.img_real[unmatch_idx]
+                label2_batch[i] = 0.
+                
+        index = real_idx
+        if (index < int(np.floor(len(self.img_data) / self.batch_size))):
+            return [img1_batch.astype(np.float32) / 255., img2_batch.astype(np.float32) / 255.], label2_batch
+        
+        return [img2_batch.astype(np.float32) / 255., img1_batch.astype(np.float32) / 255.], label2_batch
+
+    def on_epoch_end(self):
+        if self.shuffle == True:
+            self.img_data, self.label_data = shuffle(self.img_data, self.label_data)
+
+
+
+# %%
+# make data
+train_gen = DataGenerator(img_train, label_train, img_real, label_real, shuffle = True)
+val_gen = DataGenerator(img_val, label_val, img_real, label_real, shuffle = True)
+
+
+
+# %%
+# configure checkpoint data
+checkpoint_path = '../../model/checkpoint/cp.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath = checkpoint_path, save_weights_only = True, verbose = 1)
+
+
+
+# %%
+# prepare model
+model = build_model()
+if (os.path.exists(checkpoint_path + '.index')):
+    print('continue training')
+    model.load_weights(checkpoint_path)
+
+
+
+# %%
+# training model
+history = model.fit(train_gen, epochs = 50, validation_data = val_gen, callbacks = [cp_callback])
+
+
+
+# %%
+# save model
+model_path = '../../model/result/fp160.h5'
+model_dir = os.path.dirname(model_path)
+model.save(model_path)
+
+
+
+# %%
